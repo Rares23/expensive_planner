07eea007076813d9766b0da73693cf751826b256
Rares Cruceat <cruceat.rares@gmail.com>
2021-03-21 09:24:51 +0200

    set bars to take all the space in the parent

--- lib/widgets/chart_bar_view.dart
+++ lib/widgets/chart_bar_view.dart
@@ -21,32 +21,33 @@ class ChartBarView extends StatelessWidget {
             child: Text('\$${spendingAmound.toStringAsFixed(2)}'),
           ),
         ),
-        Container(
-          height: 60,
-          width: 10,
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 1.0,
-                  ),
-                  color: Theme.of(context).primaryColorLight,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: spendingPctOfTotal,
-                alignment: Alignment.bottomCenter,
-                child: Container(
+        Expanded(
+          child: Container(
+            width: 10,
+            child: Stack(
+              children: [
+                Container(
                   decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    color: Theme.of(context).primaryColorLight,
                     borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              )
-            ],
+                FractionallySizedBox(
+                  heightFactor: spendingPctOfTotal,
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
         SizedBox(height: 4),
